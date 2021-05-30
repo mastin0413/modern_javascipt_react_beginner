0edda133ada1d1244407e43d44402dbc835b4e88
Nobuo Masukawa <43978362+mastin0413@users.noreply.github.com>
2021-05-30 11:21:22 +0900

    4-19

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import "./styles.css";
+/**
+ * Const Let
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //val1を再宣言
+// var val1 = "再宣言";
+
+// //const let 基本はconstを使う、letは可変の場合のみ
+// //オブジェクトはconstで作成しても要素変更可能
+
+// const obj1 = {
+//   name:"増川",
+//   age:"39"
+// }
+// obj1.occupation = "adobe employee";
+// obj1.name = "Mastin";
+// console.log(obj1);
+
+// const ar1 = ["mastin0413", "38"];
+// console.log(ar1);
+// ar1[0] = "hogehoge";
+
+//テンプレート文字列
+// const name = "mas";
+// const age = "38";
+// const message = `私の名前は${name}です、${age}歳です`;
+// console.log(message);
+
+//アロー関数
