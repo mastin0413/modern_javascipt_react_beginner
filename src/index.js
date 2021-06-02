85e114d1f16db0a4dd063d6fddbe082bc34d9f82
Nobuo Masukawa <43978362+mastin0413@users.noreply.github.com>
2021-06-02 14:00:13 +0900

    4-25 4-25

--- src/index.js
+++ src/index.js
@@ -75,9 +75,95 @@
 
 // console.log(message2);
 
-const myProfiele = ["増川", 38];
-const [name, age] = myProfiele;
+// const myProfiele = ["増川", 38];
+// const [name, age] = myProfiele;
 
-const message4 = `名前は${name}です、年齢は${age}歳です`;
+// const message4 = `名前は${name}です、年齢は${age}歳です`;
 
-console.log(message4);
+// console.log(message4);
+
+/**
+ * デフォルト値
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// console.log(sayHello("ますかわ"));
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// //sumFunc(arr1[0], arr1[1]);
+
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+//配列のコピー、結合
+// const arr4 = [10,20];
+// const arr5 = [30,40];
+
+// const arr6 = [...arr4];
+// const arr7 = [...arr4, ...arr5];
+
+// console.log(arr6);
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
+
+// arr6[0] = 100;
+// console.log(arr4);
+// console.log(arr6);
+
+/**
+ * マップ、フィルターを使った配列の処理
+ */
+const nameArr = ["西村", "森田", "増川"];
+
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index}番目は${nameArr[index]}です。`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   name = name + "ぽぽ";
+//   return name
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, i) => console.log(`${i}は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+/**
+ * 3項演算子
+ */
+
+// ある条件 ? 条件がtrueの時返却 : 条件がfalseの時
+//  const val1 = 1 > 0 ? "trueです" : "falseです";
+//  console.log(val1);
+
+//  const num = 1290;
+//  console.log(num.toLocaleString());
+
+//  const formatedNum = typeof(num) === 'number' ? num.toLocaleString() : "数値を入力してください";
+//  console.log(formatedNum);
+
+// const checkSum = (num1, num2) => num1 + num2 > 100 ? "１００を超えています" : "許容範囲内です";
+// console.log(checkSum(50, 51))
